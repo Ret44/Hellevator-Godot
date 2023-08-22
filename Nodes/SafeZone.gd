d18703fb3bb7f15fb1fbcb2686a99c842c766d55
Bartłomiej Sieczka <bartlomiej.sieczka@crunchingkoalas.com>
2023-08-23 01:48:08 +0200

    Fixed safe zone trigger area

--- Nodes/SafeZone.gd
+++ Nodes/SafeZone.gd
@@ -1,5 +1,6 @@
 extends Area2D
 
+@export var kick_direction : float
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -12,5 +13,9 @@ func _process(delta):
 
 
 func _on_body_entered(body):
-	print(body.name + " entered " + self.name)
+	if(body.is_in_group("Guests")):
+		print(body.name + " entered " + self.name)
+		body.apply_impulse(Vector2(1000 * kick_direction, 0))
+		Sounds.play(Sounds.cash)
+		pass
 	pass # Replace with function body.

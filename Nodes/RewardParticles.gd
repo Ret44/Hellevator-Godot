1adeea79325715cba0c8df0c7d42776363d65667
Bartłomiej Sieczka <bartlomiej.sieczka@crunchingkoalas.com>
2023-08-23 10:36:49 +0200

    Coin and Money particle prefab nodes

--- Nodes/RewardParticles.gd
+++ Nodes/RewardParticles.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 @export var emitters = []
-
+@export var lifetime : float
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	for i in range(0, emitters.size()):
@@ -17,4 +17,8 @@ func emit():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	lifetime -= delta
+	if(lifetime <= 0):
+		free()
+		pass
 	pass
